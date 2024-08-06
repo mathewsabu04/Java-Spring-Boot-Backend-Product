2614127e3f4c50308fc4d5195da9e31b0ad8e0cc
Mathew Sabu <internshipmathewsabu04@gmail.com>
2024-08-06 10:51:32 -0400

    Created a default constructor for the ProfanityFilterAPIResponse

--- src/main/java/com/example/FinalExamProject/Exception/ProfanityFilterAPIResponse.java
+++ src/main/java/com/example/FinalExamProject/Exception/ProfanityFilterAPIResponse.java
@@ -7,14 +7,17 @@ import lombok.ToString;
 @Getter
 @ToString
 @Setter
-
 public class ProfanityFilterAPIResponse {
     private String original;
     private String censored;
     private boolean has_profanity;
 
+    // Default constructor
+    public ProfanityFilterAPIResponse() {
+    }
+
+    // Constructor with parameters
     public ProfanityFilterAPIResponse(boolean has_profanity) {
         this.has_profanity = has_profanity;
-
     }
 }
